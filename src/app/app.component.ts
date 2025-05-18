ebd5df3c5ebd04ea06e815b05d0d57187aeb7d37
Magdalena Witt <magdalena.zaporska@gmail.com>
2025-05-18 19:21:43 +0200

    Autocomplete for filtering proceedings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,22 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { PieChartComponent } from '../components/pie-chart/pie-chart.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { TermService } from '../services/term.service';
 import { ProceedingService } from '../services/proceeding.service';
 import { Proceeding } from '../model/proceeding';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { map, Observable, startWith } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  imports: [MatTableModule, MatDividerModule, MatExpansionModule, PieChartComponent, MatSelectModule, FormsModule, MatFormFieldModule, MatSnackBarModule, MatProgressSpinnerModule],
+  imports: [MatTableModule, MatDividerModule, MatExpansionModule, PieChartComponent, MatSelectModule, FormsModule, MatFormFieldModule, MatSnackBarModule,
+    MatProgressSpinnerModule, MatAutocompleteModule, ReactiveFormsModule, MatInputModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -31,6 +36,8 @@ export class AppComponent implements OnInit {
   //proceedings
   proceedings = signal<Proceeding[]>([]);
   selectedProceeding = signal<Proceeding | undefined>(undefined);
+  proceedingControl = new FormControl<string | Proceeding>('');
+  filteredProceedings: Observable<Proceeding[]> | undefined;
 
   //votings
   votings = signal<Voting[]>([]);
@@ -41,7 +48,7 @@ export class AppComponent implements OnInit {
   showDetailedVotingResults = signal<boolean>(false);
   detailedVotingResultsColumns: string[] = ['first-name', 'last-name', 'club', 'voted'];
 
-  // Loading state
+  //loading state
   isLoading = signal<boolean>(false);
 
   ngOnInit(): void {
@@ -52,13 +59,13 @@ export class AppComponent implements OnInit {
         this.terms.set(terms);
         this.selectedTerm.set(terms[0]);
         this.onTermSelected(this.selectedTerm()!)
-        console.log('Terms fetched successfully:', terms);
       },
       error: (error) => {
         console.error('No terms available', error);
-        this.openSnackBar("Nie ma dostępnych kadencji.");
+        this._openSnackBar("Nie ma dostępnych kadencji.");
       },
       complete: () => {
+        this._filterProceedings();
         this.isLoading.set(false);
       }
     });
@@ -70,6 +77,7 @@ export class AppComponent implements OnInit {
     this.selectedProceeding.set(undefined);
     this.votings.set([]);
     this.selectedVoting.set(undefined);
+    this.proceedingControl.setValue('');
 
     // Getting proceedings for the selected term
     this.isLoading.set(true);
@@ -77,17 +85,17 @@ export class AppComponent implements OnInit {
       next: (proceedings) => {
         if (proceedings.length != 0) {
           this.proceedings.set(proceedings);
-          console.log('Proceedings fetched successfully:', proceedings);
         } else {
           console.error('No proceedings available for the selected term');
-          this.openSnackBar("Nie ma dostępnych posiedzeń.");
+          this._openSnackBar("Nie ma dostępnych posiedzeń.");
         }
       },
       error: (error) => {
         console.error('Error fetching proceedings:', error);
-        this.openSnackBar("Nie ma dostępnych posiedzeń.");
+        this._openSnackBar("Nie ma dostępnych posiedzeń.");
       },
       complete: () => {
+        this._filterProceedings();
         this.isLoading.set(false);
       }
     });
@@ -108,15 +116,14 @@ export class AppComponent implements OnInit {
         if (votings.length != 0) {
           this.votings.set(votings);
           this.selectedVoting.set(undefined);
-          console.log('Votings fetched successfully:', votings);
         } else {
           console.error('No votings available for the selected term and proceeding');
-          this.openSnackBar("Nie ma dostępnych głosowań.");
+          this._openSnackBar("Nie ma dostępnych głosowań.");
         }
       },
       error: (error) => {
         console.error('Error fetching votings:', error);
-        this.openSnackBar("Nie ma dostępnych głosowań.");
+        this._openSnackBar("Nie ma dostępnych głosowań.");
       },
       complete: () => {
         this.isLoading.set(false);
@@ -134,15 +141,14 @@ export class AppComponent implements OnInit {
       next: (votingResults) => {
         if (votingResults) {
           this.votingResults.set(votingResults);
-          console.log('Voting data fetched successfully:', votingResults);
         } else {
           console.error('No voting data available for the selected term, proceeding, and voting');
-          this.openSnackBar("Nie ma dostępnych danych głosowania.");
+          this._openSnackBar("Nie ma dostępnych danych głosowania.");
         }
       },
       error: (error) => {
         console.error('Error fetching voting data:', error);
-        this.openSnackBar("Nie ma dostępnych danych głosowania.");
+        this._openSnackBar("Nie ma dostępnych danych głosowania.");
       },
       complete: () => {
         this.isLoading.set(false);
@@ -150,11 +156,30 @@ export class AppComponent implements OnInit {
     });
   }
 
-  private openSnackBar(message: string) {
+  displayProceedings(proceeding: Proceeding): string {
+    return proceeding && proceeding.title ? proceeding.title : '';
+  }
+
+  private _openSnackBar(message: string) {
     this.snackBar.open(message, "Zamknij", {
       duration: 2000,
       horizontalPosition: 'center',
       verticalPosition: 'top',
     });
+  }
+
+  private _filterProceedings() {
+    this.filteredProceedings = this.proceedingControl.valueChanges.pipe(
+      startWith(undefined),
+      map(filteringInput => {
+        const input = typeof filteringInput === 'string' ? filteringInput : filteringInput?.title;
+        return input ? this._filter(input) : this.proceedings().slice();
+      }),
+    );
+  }
+
+  private _filter(filteringInput: string): Proceeding[] {
+    const input = filteringInput.toLowerCase();
+    return this.proceedings().filter(option => option.title.toLowerCase().includes(input));
   }
 }
