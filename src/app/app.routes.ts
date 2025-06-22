8f69318848fd0ce14ed3d9ddda0a1fa8bee3733e
Magdalena Witt <magdalena.zaporska@gmail.com>
2025-06-22 20:14:04 +0200

    [dev] home-component reworked - init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,28 @@
 import { Routes } from '@angular/router';
 import { VotingsComponent } from '../pages/votings/votings.component';
 import { VotingDetailsComponent } from '../pages/voting-details/voting-details.component';
+import { HomeComponent } from '../pages/home/home.component';
 
 
 export const homeRoute = `home`
+export const votingsRoute = `votings`
 export const votingDetailsRoute = `details/:id`
 
 export const routes: Routes = [
     {
         path: '',
-        component: VotingsComponent,
+        component: HomeComponent,
         title: 'Stronga główna',
     },
     {
         path: homeRoute,
-        component: VotingsComponent,
+        component: HomeComponent,
         title: 'Stronga główna',
+    },
+    {
+        path: votingsRoute,
+        component: VotingsComponent,
+        title: 'Głosowania',
     },
     {
         path: votingDetailsRoute,
