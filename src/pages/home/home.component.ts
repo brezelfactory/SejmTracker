86668c0bcec32a6a825bb922c930940ddadf3d07
Magdalena Witt <magdalena.zaporska@gmail.com>
2025-06-29 20:23:25 +0200

    [dev] Home page header added

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-home',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule, MatDividerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
